f4d452503eeaba3433855fa8909030d249bd8f8b
abhisheks@prophecy.io <abhisheks@prophecy.io>
2024-11-15 06:37:38 +0000

    Enable dependency execution in REL_DB_MWAA job configuration

--- jobs/REL_DB_MWAA/code/uitesting_shared_sql_childdatabricksshared_rel_db_mwaa/tasks/DBT_Useall.py
+++ jobs/REL_DB_MWAA/code/uitesting_shared_sql_childdatabricksshared_rel_db_mwaa/tasks/DBT_Useall.py
@@ -13,7 +13,7 @@ def DBT_Useall():
         op_kwargs = {
           "is_adhoc_run_from_same_project": False,
           "is_prophecy_managed": False,
-          "run_deps": False,
+          "run_deps": True,
           "run_seeds": False,
           "run_parents": False,
           "run_children": False,
