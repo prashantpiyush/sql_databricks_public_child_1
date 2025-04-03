24379e527af00ca56cdbb8177a7f3ede828d5c2b
abhisheks@prophecy.io <abhisheks@prophecy.io>
2025-04-03 08:33:35 +0000

    Update DBT_0.py to change DBT profile from run_profile_snowflake to run_profile in REL_DB_AIRFLOW job

--- jobs/REL_DB_AIRFLOW/code/uitesting_shared_team_sql_databrickssharedbasic_rel_db_airflow/tasks/DBT_0.py
+++ jobs/REL_DB_AIRFLOW/code/uitesting_shared_team_sql_databrickssharedbasic_rel_db_airflow/tasks/DBT_0.py
@@ -29,7 +29,7 @@ def DBT_0():
           "select": "",
           "threads": "1",
           "exclude": "",
-          "run_props": " --profile run_profile_snowflake",
+          "run_props": " --profile run_profile",
           "envs": {
             "DBT_DATABRICKS_INVOCATION_ENV": "prophecy", 
             "DBT_PROFILES_DIR": "/home/airflow/gcs/data", 
