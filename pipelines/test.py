0ca83aed181a815bf4bba73b23b6fab3adf29605
abhisheks@prophecy.io <abhisheks@prophecy.io>
2025-04-14 06:19:47 +0000

    Remove test pipeline and model for web page catalog join.

--- pipelines/test.py
+++
@@ -1,6 +0,0 @@
-with DAG():
-    model_test_web_page_catalog_join = Task(
-        task_id = "model_test_web_page_catalog_join", 
-        component = "Model", 
-        modelName = "model_test_web_page_catalog_join"
-    )
